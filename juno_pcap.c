f8834b3c4171a4101739c339f6d7fd72667e6590
junorouse <junorouse@gmail.com>
2017-07-13 14:21:29 +0900

    fix: indent

--- juno_pcap.c
+++ juno_pcap.c
@@ -10,98 +10,98 @@
 pcap_t *p;
 
 void DumpHex(const void* data, unsigned int size) {
-	char ascii[17];
-	size_t i, j;
-	ascii[16] = '\0';
-	for (i = 0; i < size; ++i) {
-		printf("%02X ", ((unsigned char*)data)[i]);
-		if (((unsigned char*)data)[i] >= ' ' && ((unsigned char*)data)[i] <= '~') {
-			ascii[i % 16] = ((unsigned char*)data)[i];
-		} else {
-			ascii[i % 16] = '.';
-		}
-		if ((i+1) % 8 == 0 || i+1 == size) {
-			printf(" ");
-			if ((i+1) % 16 == 0) {
-				printf("|  %s \n", ascii);
-			} else if (i+1 == size) {
-				ascii[(i+1) % 16] = '\0';
-				if ((i+1) % 16 <= 8) {
-					printf(" ");
-				}
-				for (j = (i+1) % 16; j < 16; ++j) {
-					printf("   ");
-				}
-				printf("|  %s \n", ascii);
-			}
-		}
-	}
+    char ascii[17];
+    size_t i, j;
+    ascii[16] = '\0';
+    for (i = 0; i < size; ++i) {
+        printf("%02X ", ((unsigned char*)data)[i]);
+        if (((unsigned char*)data)[i] >= ' ' && ((unsigned char*)data)[i] <= '~') {
+            ascii[i % 16] = ((unsigned char*)data)[i];
+        } else {
+            ascii[i % 16] = '.';
+        }
+        if ((i+1) % 8 == 0 || i+1 == size) {
+            printf(" ");
+            if ((i+1) % 16 == 0) {
+                printf("|  %s \n", ascii);
+            } else if (i+1 == size) {
+                ascii[(i+1) % 16] = '\0';
+                if ((i+1) % 16 <= 8) {
+                    printf(" ");
+                }
+                for (j = (i+1) % 16; j < 16; ++j) {
+                    printf("   ");
+                }
+                printf("|  %s \n", ascii);
+            }
+        }
+    }
 }
 
 /* Ethernet addresses are 6 bytes */
-#define ETHER_ADDR_LEN	6
+#define ETHER_ADDR_LEN  6
 
-	/* Ethernet header */
-	struct sniff_ethernet {
-		u_char ether_dhost[ETHER_ADDR_LEN]; /* Destination host address */
-		u_char ether_shost[ETHER_ADDR_LEN]; /* Source host address */
-		u_short ether_type; /* IP? ARP? RARP? etc */
-	};
+    /* Ethernet header */
+    struct sniff_ethernet {
+        u_char ether_dhost[ETHER_ADDR_LEN]; /* Destination host address */
+        u_char ether_shost[ETHER_ADDR_LEN]; /* Source host address */
+        u_short ether_type; /* IP? ARP? RARP? etc */
+    };
 
-	/* IP header */
-	struct sniff_ip {
-		u_char ip_vhl;		/* version << 4 | header length >> 2 */
-		u_char ip_tos;		/* type of service */
-		u_short ip_len;		/* total length */
-		u_short ip_id;		/* identification */
-		u_short ip_off;		/* fragment offset field */
-	#define IP_RF 0x8000		/* reserved fragment flag */
-	#define IP_DF 0x4000		/* dont fragment flag */
-	#define IP_MF 0x2000		/* more fragments flag */
-	#define IP_OFFMASK 0x1fff	/* mask for fragmenting bits */
-		u_char ip_ttl;		/* time to live */
-		u_char ip_p;		/* protocol */
-		u_short ip_sum;		/* checksum */
+    /* IP header */
+    struct sniff_ip {
+        u_char ip_vhl;      /* version << 4 | header length >> 2 */
+        u_char ip_tos;      /* type of service */
+        u_short ip_len;     /* total length */
+        u_short ip_id;      /* identification */
+        u_short ip_off;     /* fragment offset field */
+    #define IP_RF 0x8000        /* reserved fragment flag */
+    #define IP_DF 0x4000        /* dont fragment flag */
+    #define IP_MF 0x2000        /* more fragments flag */
+    #define IP_OFFMASK 0x1fff   /* mask for fragmenting bits */
+        u_char ip_ttl;      /* time to live */
+        u_char ip_p;        /* protocol */
+        u_short ip_sum;     /* checksum */
         u_int ip_src;
         u_int ip_dst;
-	};
-	#define IP_HL(ip)		(((ip)->ip_vhl) & 0x0f)
-	#define IP_V(ip)		(((ip)->ip_vhl) >> 4)
+    };
+    #define IP_HL(ip)       (((ip)->ip_vhl) & 0x0f)
+    #define IP_V(ip)        (((ip)->ip_vhl) >> 4)
 
-	/* TCP header */
-	typedef u_int tcp_seq;
+    /* TCP header */
+    typedef u_int tcp_seq;
 
-	struct sniff_tcp {
-		u_short th_sport;	/* source port */
-		u_short th_dport;	/* destination port */
-		tcp_seq th_seq;		/* sequence number */
-		tcp_seq th_ack;		/* acknowledgement number */
-		u_char th_offx2;	/* data offset, rsvd */
-	#define TH_OFF(th)	(((th)->th_offx2 & 0xf0) >> 4)
-		u_char th_flags;
-	#define TH_FIN 0x01
-	#define TH_SYN 0x02
-	#define TH_RST 0x04
-	#define TH_PUSH 0x08
-	#define TH_ACK 0x10
-	#define TH_URG 0x20
-	#define TH_ECE 0x40
-	#define TH_CWR 0x80
-	#define TH_FLAGS (TH_FIN|TH_SYN|TH_RST|TH_ACK|TH_URG|TH_ECE|TH_CWR)
-		u_short th_win;		/* window */
-		u_short th_sum;		/* checksum */
-		u_short th_urp;		/* urgent pointer */
+    struct sniff_tcp {
+        u_short th_sport;   /* source port */
+        u_short th_dport;   /* destination port */
+        tcp_seq th_seq;     /* sequence number */
+        tcp_seq th_ack;     /* acknowledgement number */
+        u_char th_offx2;    /* data offset, rsvd */
+    #define TH_OFF(th)  (((th)->th_offx2 & 0xf0) >> 4)
+        u_char th_flags;
+    #define TH_FIN 0x01
+    #define TH_SYN 0x02
+    #define TH_RST 0x04
+    #define TH_PUSH 0x08
+    #define TH_ACK 0x10
+    #define TH_URG 0x20
+    #define TH_ECE 0x40
+    #define TH_CWR 0x80
+    #define TH_FLAGS (TH_FIN|TH_SYN|TH_RST|TH_ACK|TH_URG|TH_ECE|TH_CWR)
+        u_short th_win;     /* window */
+        u_short th_sum;     /* checksum */
+        u_short th_urp;     /* urgent pointer */
 };
 
 #define SIZE_ETHERNET 14
 
-	const struct sniff_ethernet *ethernet; /* The ethernet header */
-	const struct sniff_ip *ip; /* The IP header */
-	const struct sniff_tcp *tcp; /* The TCP header */
-	const char *payload; /* Packet payload */
+    const struct sniff_ethernet *ethernet; /* The ethernet header */
+    const struct sniff_ip *ip; /* The IP header */
+    const struct sniff_tcp *tcp; /* The TCP header */
+    const char *payload; /* Packet payload */
 
-	u_int size_ip;
-	u_int size_tcp;
+    u_int size_ip;
+    u_int size_tcp;
 
 
 
@@ -114,20 +114,20 @@ void got_packet(u_char *args, const struct pcap_pkthdr *header,
 int main(int argc, char **argv) {
     char *dev;
     char errbuf[PCAP_ERRBUF_SIZE];
-	dev = pcap_lookupdev(errbuf);
+    dev = pcap_lookupdev(errbuf);
 
-	if (dev == NULL) {
-		fprintf(stderr, "Couldn't find default device: %s\n", errbuf);
-		return(2);
-	}
+    if (dev == NULL) {
+        fprintf(stderr, "Couldn't find default device: %s\n", errbuf);
+        return(2);
+    }
 
-	printf("Device: %s\n", dev);
+    printf("Device: %s\n", dev);
     p = pcap_open_live(dev, 16384, 1, 200, errbuf);
 
-	if (p == NULL) {
-		fprintf(stderr, "Couldn't open pcap: %s\n", errbuf);
-		return(2);
-	}
+    if (p == NULL) {
+        fprintf(stderr, "Couldn't open pcap: %s\n", errbuf);
+        return(2);
+    }
 
     printf("p -> %p\n", p);
 
@@ -153,19 +153,19 @@ void got_packet(u_char *args, const struct pcap_pkthdr *header,
     unsigned short ether_type, sport, dport;
 
     ethernet = (struct sniff_ethernet*)(packet);
-	ip = (struct sniff_ip*)(packet + SIZE_ETHERNET);
-	size_ip = IP_HL(ip)*4;
-	if (size_ip < 20) {
-		// printf("\x1B[96m* Invalid IP header length: %u bytes\n", size_ip);
-		return;
-	}
-	tcp = (struct sniff_tcp*)(packet + SIZE_ETHERNET + size_ip);
-	size_tcp = TH_OFF(tcp)*4;
-	if (size_tcp < 20) {
-		// printf("\x1B[96m* Invalid TCP header length: %u bytes\n", size_tcp);
-		return;
-	}
-	payload = (u_char *)(packet + SIZE_ETHERNET + size_ip + size_tcp);
+    ip = (struct sniff_ip*)(packet + SIZE_ETHERNET);
+    size_ip = IP_HL(ip)*4;
+    if (size_ip < 20) {
+        // printf("\x1B[96m* Invalid IP header length: %u bytes\n", size_ip);
+        return;
+    }
+    tcp = (struct sniff_tcp*)(packet + SIZE_ETHERNET + size_ip);
+    size_tcp = TH_OFF(tcp)*4;
+    if (size_tcp < 20) {
+        // printf("\x1B[96m* Invalid TCP header length: %u bytes\n", size_tcp);
+        return;
+    }
+    payload = (u_char *)(packet + SIZE_ETHERNET + size_ip + size_tcp);
 
     puts("\x1B[32m============== Packet ==============\x1B[0m");
 
