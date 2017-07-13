919103f01e8b4241db8a0522557209196a62c292
junorouse <junorouse@gmail.com>
2017-07-13 14:01:07 +0900

    feature: hexdump data

--- juno_pcap.c
+++ juno_pcap.c
@@ -9,6 +9,35 @@
 
 pcap_t *p;
 
+void DumpHex(const void* data, unsigned int size) {
+	char ascii[17];
+	size_t i, j;
+	ascii[16] = '\0';
+	for (i = 0; i < size; ++i) {
+		printf("%02X ", ((unsigned char*)data)[i]);
+		if (((unsigned char*)data)[i] >= ' ' && ((unsigned char*)data)[i] <= '~') {
+			ascii[i % 16] = ((unsigned char*)data)[i];
+		} else {
+			ascii[i % 16] = '.';
+		}
+		if ((i+1) % 8 == 0 || i+1 == size) {
+			printf(" ");
+			if ((i+1) % 16 == 0) {
+				printf("|  %s \n", ascii);
+			} else if (i+1 == size) {
+				ascii[(i+1) % 16] = '\0';
+				if ((i+1) % 16 <= 8) {
+					printf(" ");
+				}
+				for (j = (i+1) % 16; j < 16; ++j) {
+					printf("   ");
+				}
+				printf("|  %s \n", ascii);
+			}
+		}
+	}
+}
+
 /* Ethernet addresses are 6 bytes */
 #define ETHER_ADDR_LEN	6
 
@@ -123,7 +152,7 @@ void got_packet(u_char *args, const struct pcap_pkthdr *header,
     unsigned int tcp_seq;
     unsigned short ether_type, sport, dport;
 
-ethernet = (struct sniff_ethernet*)(packet);
+    ethernet = (struct sniff_ethernet*)(packet);
 	ip = (struct sniff_ip*)(packet + SIZE_ETHERNET);
 	size_ip = IP_HL(ip)*4;
 	if (size_ip < 20) {
@@ -193,7 +222,12 @@ ethernet = (struct sniff_ethernet*)(packet);
 
     hIdx += 4;
 
-    puts(payload);
+    // puts(payload);
+    printf("caplen: %d\n", header->caplen);
+    printf("wtf: %d\n", SIZE_ETHERNET + size_ip + size_tcp);
+    int realSize = header->caplen - (SIZE_ETHERNET + size_ip + size_tcp);
+    if (!(realSize <= 0))
+        DumpHex(payload, header->caplen);
 
 
 }
